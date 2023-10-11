471214fbfcf9b349124ce805bf935ce910db9dd7
Gnems19 <71854341+Gnems19@users.noreply.github.com>
2023-10-11 21:26:33 +0400

    Add Distinct with tests Add Count with tests Add Skip with tests Add Take with tests Add Agregate with tests Add Min with tests Add Max with tests Add FirstOrDefault with tests

--- MyLinqs/MyLinq.cs
+++ MyLinqs/MyLinq.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
+
 namespace MyLinqs
 {
     public class MyLinq<T> : IEnumerable<T>
@@ -20,7 +22,7 @@ namespace MyLinqs
             _enumerator = enumerator ?? throw new ArgumentNullException(nameof(enumerator));
         }
 
-        public MyLinq<T> Where(Func<T, bool> predicate)
+        public MyLinq<T> Where(Predicate<T> predicate)
         {
             if (predicate == null)
             {
@@ -30,8 +32,8 @@ namespace MyLinqs
         }
 
         // creates a specific enumerable without creating an actual enumerable class and then ovveriding its enumerator by hand 
-        // just giving the iteration logic with a method
-        private IEnumerable<T> WhereEnumerable(Func<T, bool> predicate)
+        // just giving the iteration logic
+        private IEnumerable<T> WhereEnumerable(Predicate<T> predicate)
         {
             while (_enumerator.MoveNext())
             {
@@ -73,6 +75,147 @@ namespace MyLinqs
                 yield return _enumerator.Current;
             }
         }
+
+        public MyLinq<T> Skip(int count)
+        {
+            return new MyLinq<T>(SkipEnumerable(count));
+        }
+
+        private IEnumerable<T> SkipEnumerable(int count)
+        {
+            while (count > 0 && _enumerator.MoveNext())
+            {
+                count--;
+            }
+            while (_enumerator.MoveNext())
+            {
+                yield return _enumerator.Current;
+            }
+        }
+
+        public T? Agregate(Func<T, T, T> acumulator)
+        {
+            if (acumulator == null)
+            {
+                throw new ArgumentNullException(nameof(acumulator));
+            }
+            if (!_enumerator.MoveNext())
+            {
+                return default;
+            }
+            var result = _enumerator.Current;
+            while (_enumerator.MoveNext())
+            {
+                result = acumulator(result, _enumerator.Current);
+            }
+            return result;
+        }
+
+        public T Agregate(T seed, Func<T, T, T> acumulator)
+        {
+            if (acumulator == null)
+            {
+                throw new ArgumentNullException(nameof(acumulator));
+            }
+            var result = seed;
+            while (_enumerator.MoveNext())
+            {
+                result = acumulator(result, _enumerator.Current);
+            }
+            return result;
+        }
+
+        public TAccumulate Agregate<TAccumulate>(TAccumulate seed, Func<TAccumulate, T, TAccumulate> acumulator)
+        {
+            if(acumulator == null)
+            {
+                throw new ArgumentNullException(nameof(acumulator));
+            }
+            var result = seed;
+            while (_enumerator.MoveNext())
+            {
+                result = acumulator(result, _enumerator.Current);
+            }
+            return result;
+        }
+
+        public long Count()
+        {
+            long count = 0;
+            while (_enumerator.MoveNext())
+            {
+                count++;
+            }
+            return count;
+        }
+
+        public long Count(Predicate<T> predicate)
+        {
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+            long count = 0;
+            while (_enumerator.MoveNext())
+            {
+                if(predicate(_enumerator.Current))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public T? FirstOrDefault(Predicate<T> predicate)
+        {
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+            while (_enumerator.MoveNext())
+            {
+                if(predicate(_enumerator.Current))
+                {
+                    return _enumerator.Current;
+                }
+            }
+            return default;
+        }
+
+        public MyLinq<T> Distinct()
+        {
+            return new MyLinq<T>(DistinctEnumerable());
+        }
+
+        private IEnumerable<T> DistinctEnumerable()
+        {
+            HashSet<T> distinctValues = new();
+            while (_enumerator.MoveNext())
+            {
+                if(distinctValues.Add(_enumerator.Current))
+                {
+                    yield return _enumerator.Current;
+                }
+            }
+        }
+        
+        public T? Max(Comparer<T> comparer)
+        {
+            if(comparer == null)
+            {
+                throw new ArgumentNullException(nameof(comparer));
+            }
+            return Agregate((x, y) => comparer.Compare(x, y) > 0 ? x : y);
+        }
+        public T? Min(Comparer<T> comparer)
+        {
+            if (comparer == null)
+            {
+                throw new ArgumentNullException(nameof(comparer));
+            }
+            return Agregate((x, y) => comparer.Compare(x, y) < 0 ? x : y);
+        }
+
 
         public IEnumerator<T> GetEnumerator()
         {
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -19,7 +19,6 @@ namespace Tests
             {
                 actual.MoveNext();
                 Assert.Equal(item, actual.Current);
-                
             }
         }
         [Fact]
@@ -35,6 +34,108 @@ namespace Tests
             }
         }
 
+        [Fact]
+        public void TestSelectWhere()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5 };
+            var expected = list.Select(x => x * 2).Where(x => x % 2 == 0);
+            var actual = new MyLinq<int>(list).Select(x => x * 2).Where(x => x % 2 == 0).GetEnumerator();
+            foreach (var item in expected)
+            {
+                actual.MoveNext();
+                Assert.Equal(item, actual.Current);
+            }
+        }
+        [Fact]
+        public void TestWhereSelect()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5 };
+            var expected = list.Where(x => x % 2 == 0).Select(x => x * 2);
+            var actual = new MyLinq<int>(list).Where(x => x % 2 == 0).Select(x => x * 2).GetEnumerator();
+            foreach (var item in expected)
+            {
+                actual.MoveNext();
+                Assert.Equal(item, actual.Current);
+            }
+        }
+        [Fact]
+        public void TestTake()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5 };
+            var expected = list.Take(3);
+            var actual = new MyLinq<int>(list).Take(3).GetEnumerator();
+            foreach (var item in expected)
+            {
+                actual.MoveNext();
+                Assert.Equal(item, actual.Current);
+            }
+        }
+        [Fact]
+        public void TestAgregate()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5 };
+            var expected = list.Aggregate((x, y) => x + y);
+            var actual = new MyLinq<int>(list).Aggregate((x, y) => x + y);
+            Assert.Equal(expected, actual);
 
+            var list1 = new List<int> { 1, 2, 3, 4, 5 };
+            var expected1 = list1.Aggregate(0, (x, y) => x + y);
+            var actual1 = new MyLinq<int>(list1).Aggregate(0, (x, y) => x + y);
+            Assert.Equal(expected1, actual1);
+
+            var evenEvensList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+            var expected2 = evenEvensList.Aggregate(true, (x, y) => x ^ y % 2 == 0);
+            var actual2 = new MyLinq<int>(evenEvensList).Aggregate(true, (x, y) => x ^ y % 2 == 0);
+            Assert.Equal(expected2, actual2);
+        }
+        [Fact]
+        public void TestCount()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5 };
+            var expected = list.Count();
+            var actual = new MyLinq<int>(list).Count();
+            Assert.Equal(expected, actual);
+
+            var list1 = new List<int> { 1, 2, 3, 4, 5 };
+            var expected1 = list1.Count(x => x % 2 == 0);
+            var actual1 = new MyLinq<int>(list1).Count(x => x % 2 == 0);
+            Assert.Equal(expected1, actual1);
+        }
+        [Fact] public void TestFirstOrDefault()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5 };
+            var expected = list.FirstOrDefault();
+            var actual = new MyLinq<int>(list).FirstOrDefault();
+            Assert.Equal(expected, actual);
+            var list1 = new List<int> { };
+            var expected1 = list1.FirstOrDefault();
+            var actual1 = new MyLinq<int>(list1).FirstOrDefault();
+            Assert.Equal(expected1, actual1);
+        }
+
+        [Fact]
+        public void TestDistinct()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 99, 999};
+            var expected = list.Distinct();
+            var actual = new MyLinq<int>(list).Distinct().GetEnumerator();
+            foreach (var item in expected)
+            {
+                actual.MoveNext();
+                Assert.Equal(item, actual.Current);
+            }
+        }
+
+        [Fact]
+        public void TestMinMax()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, -11};
+            var expected = list.Min();
+            var actual = new MyLinq<int>(list).Min();
+            Assert.Equal(expected, actual);
+            expected = list.Max();
+            actual = new MyLinq<int>(list).Max();
+            Assert.Equal(expected, actual);
+        }
     }
 }
